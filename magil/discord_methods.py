7fc8c98b3aea9dbf41ecfbbf10734b6221b9ffa3
andersonmemory <andersonleitedesa2100@gmail.com>
2025-02-18 21:38:19 -0300

    feat: connects webhook to magil

---
+++ magil/discord_methods.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+from discord import Webhook
+import aiohttp
+
+
+load_dotenv()
+
+async def webhook_send(message, webhook_name):
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(os.getenv('WEBHOOK_COMMANDS'), session=session)
+        await webhook.send(message, username=webhook_name)
