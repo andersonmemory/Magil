085f3125a716f779b4dfc73e4cd6d58c418f0fc6
andersonmemory <andersonleitedesa2100@gmail.com>
2025-02-18 21:38:19 -0300

    feat: connects webhook to discord

--- magil/views.py
+++ magil/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+
+import asyncio
+
+from . import discord_methods
+
+
 def home(request):
     return render(request, 'home.html')
+
+def training(request):
+
+    message = "An user just entered on the Magil website!"
+    webhook_name = "Magil logs"
+
+    asyncio.run(discord_methods.webhook_send(message, webhook_name))
+
+
+    return render(request, 'training.html')
