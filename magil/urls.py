4c739c8563474da5ea3059215d6ed86156ea7ff2
andersonmemory <andersonleitedesa2100@gmail.com>
2025-02-23 13:02:54 -0300

    fix: fix server error

--- magil/urls.py
+++ magil/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('training/', views.training)
+    path('training/', views.training),
 ]
--- magil/views.py
+++ magil/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 
-
 import asyncio
 
 from . import discord_methods
-
 
 def home(request):
     return render(request, 'home.html')
@@ -14,9 +12,6 @@ def training(request):
     message = "An user just entered on the Magil website!"
     webhook_name = "Magil logs"
 
-    print(os.getenv('WEBHOOK_COMMANDS'))
-
     asyncio.run(discord_methods.webhook_send(message, webhook_name))
-
 
     return render(request, 'training.html')
