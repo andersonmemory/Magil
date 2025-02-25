5cf9e628e4f69e5221036b9ab7ffd1db48079e03
andersonmemory <andersonleitedesa2100@gmail.com>
2025-02-25 15:18:17 -0300

    fix: change functions

--- magil_math/views.py
+++ magil_math/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'magil_math/magil_math.html')
+
+# TODO
+def sum(request):
+    pass
+
+def subtraction(request):
+    pass
+
+def multiplication(request):
+    pass
+
+def division(request):
+    pass
