0595c31ffedb9e2cfc206639a73e6ccc5bc6736d
andersonmemory <andersonleitedesa2100@gmail.com>
2025-03-10 21:56:19 -0300

    feat: add cards url

--- magil_memory/views.py
+++ magil_memory/views.py
@@ -19,6 +19,26 @@ def digits(request):
     if request.method == "POST":
 
         user_value = request.POST.get('discord_id')
+        webhook_name = "Disciplina de cartas"
+
+        memo_elapsed = request.POST.get('memo_elapsed')
+        recall_elapsed = request.POST.get('recall_elapsed')
+        digits_amount = request.POST.get('cards_amount')
+        user_score = request.POST.get("user_score")
+        user_failure = request.POST.get("user_failure")
+
+        if user_failure == '0':
+            asyncio.run(webhook_send(f"<@{user_value}> memorizou {user_score} dígitos sem nenhum erro em {memo_elapsed}, com {recall_elapsed} de recall!", webhook_name))
+        else:
+            asyncio.run(webhook_send(f"<@{user_value}> memorizou {user_score} de {digits_amount} dígitos em {memo_elapsed}, com {recall_elapsed} de recall!", webhook_name))
+
+    return render(request, 'magil_memory/cards.html')
+
+# TODO: /cards
+def cards(request):
+    if request.method == "POST":
+
+        user_value = request.POST.get('discord_id')
         webhook_name = "Disciplina de dígitos"
 
         memo_elapsed = request.POST.get('memo_elapsed')
@@ -32,9 +52,7 @@ def digits(request):
         else:
             asyncio.run(webhook_training_logs_send(f"<@{user_value}> memorizou {user_score} de {digits_amount} dígitos em {memo_elapsed}, com {recall_elapsed} de recall!", webhook_name))
 
-    return render(request, 'magil_memory/digits.html')
-
-# TODO: /cards
+    return render(request, 'magil_memory/cards.html')
 
 # TODO: /spoken_digits
 
