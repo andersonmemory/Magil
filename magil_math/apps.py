6e1d79514ceb27460502a7ce418f7f56ca30f012
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-13 19:01:37 -0300

    refactor: remove all apps

--- magil_math/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MagilMathConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'magil_math'
--- magil_memory/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.home),
-    path('digits/', views.digits, name="digits"),
-    path('cards/', views.cards, name="cards"),
-]
