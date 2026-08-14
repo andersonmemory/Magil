73438d6d89eda5add0538e2ec822dbd393b542d8
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-14 08:14:43 -0300

    feat: add mariadb database

--- magil/settings.py
+++ magil/settings.py
@@ -79,14 +79,21 @@ WSGI_APPLICATION = "magil.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": BASE_DIR / "db.cnf",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
