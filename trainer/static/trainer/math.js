588464aa4f609954f7a152061d99803aa1f79716
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-08-30 03:17:40 -0300

    feat: add technical debt and prepare first math category

--- trainer/static/trainer/math.js
+++ trainer/static/trainer/math.js
@@ -1,8 +1,27 @@
+
+const MATH_OPERATORS = ["adição", "subtração", "multiplicação", "divisão"]
+
 function mathInitialScreen() {
 
   // TODO: Finish implementation
+  const mathScreen = document.createElement('div');
+  mathScreen.style.display = 'grid';
+
+
+  // TODO: remove manual implementation and use iterative approach
+  sumBtn = document.createElement('button');
+
+  // TODO: complete implementation with chooseOperator
+  // MATH_OPERATORS.forEach((operator) => {
+  //
+  // })
 
   // HTMLElement
-  return
+  return mathScreen;
 
 }
+
+function chooseOperator(operator) {
+  // TODO: use hashmap to apply the strategy pattern
+  return
+};
