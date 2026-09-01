cb6374285355f6098f3440429ae8800a5180f781
andersonmemory <andersonleitedesa2100@protonmail.com>
2026-09-01 19:47:10 -0300

    refactor: cleaner code and minimal MVP

--- trainer/static/trainer/math.js
+++ trainer/static/trainer/math.js
@@ -1,43 +1,62 @@
-
-const MATH_OPERATORS = ["adição", "subtração", "multiplicação", "divisão"]
-
-function mathInitialScreen() {
-
-  // TODO: Finish implementation
-  const mathScreen = document.createElement('div');
-  mathScreen.style.display = 'grid';
-
-
-  // TODO: remove manual implementation and use iterative approach
-  sumBtn = document.createElement('button');
-  sumBtn.innerHTML = "addition";
-
-  sumBtn.onclick = () => {
-    mathScreen.append(CalcScreen());
-  };
-
-  mathScreen.append(sumBtn);
-
-
-  // TODO: complete implementation with chooseOperator
-  // MATH_OPERATORS.forEach((operator) => {
-  //
-  // })
-
-  // HTMLElement
-  return mathScreen;
-
-}
-
 /**
  * Creates the screen used in the calculation phase
  * 
  */
-function CalcScreen() {
+export function CalcScreen(operator) {
+
+  // TODO: the operator should change according to the parameter's value
+  console.log("Operator is: " + operator);
+
+  const panel = document.createElement('div');
+
+  const question = document.createElement('p');
+
+  const answer = document.createElement('input');
+  answer.type = 'text';
+  answer.placeHolder = 'Insira sua resposta';
+
+
+  //  TODO:: apply DRY and remove repetition
+  let additionObj = addition();
+  let result = additionObj.first + additionObj.second;
+  question.innerHTML = `${additionObj.first} + ${additionObj.second}?`;
+
+  answer.addEventListener('keyDown', (event) => {
+
+    if (event.key === 'Enter') {
+      // console.log('pressedEnter')
+      // TODO:: add handler for proper input
+      // it assumes for now that it'll always be an valid integer
+      userAnswer = parseInt(answer.value)
+
+      if (userAnswer === result) {
+        // repetition here
+        const { first, second } = addition();
+        result = additionObj.first + additionObj.second;
+        question.innerHTML = `${first} + ${second}?`;
+
+      }
+    }
+  });
+
+  panel.append(question, answer);
+  return panel;
 
 }
 
-function chooseOperator(operator) {
-  // TODO: use hashmap to apply the strategy pattern
-  return
-};
+
+function addition() {
+
+  const question = {
+    first: Math.floor(Math.random() * 10) + 1,
+    second: Math.floor(Math.random() * 10) + 1
+  };
+
+  return question;
+}
+//
+//
+// function chooseOperator(operator) {
+//   // TODO: use hashmap to apply the strategy pattern
+//   return
+// };
